3f501ec27df6c5a19bf80fce59334da94e0c004a
Sergii Zhuravel <szhuravell@gmail.com>
2022-03-11 22:25:26 +0200

    Updated padding with 0

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const updateTimerUI = (textDay, textHour, textMinute, textSecond) => {
-  document.querySelector(".day").innerText = textDay > 0 ? textDay : 0;
-  document.querySelector(".hour").innerText = textHour > 0 ? textHour : 0;
-  document.querySelector(".minute").innerText = textMinute > 0 ? textMinute : 0;
-  document.querySelector(".second").innerText = textSecond > 0 ? textSecond : 0;
+  document.querySelector(".day").innerText = textDay;
+  document.querySelector(".hour").innerText = textHour;
+  document.querySelector(".minute").innerText = textMinute;
+  document.querySelector(".second").innerText = textSecond;
 };
-const countdown = () => {
-  // Specify the date and time we are counting down to.
+
+const pad = (n) => {
+  const str = n.toString();
+  return `${str.length === 1 ? "0" : ""}${n}`;
+};
+
+const tick = () => {
   const warStartTime = new Date("Feb 24, 2022 05:00:00").getTime();
   const now = new Date().getTime();
   const remainingTime = now - warStartTime;
@@ -16,11 +21,11 @@ const countdown = () => {
   const day = hour * 24;
 
   const textDay = Math.floor(remainingTime / day);
-  const textHour = Math.floor((remainingTime % day) / hour);
-  const textMinute = Math.floor((remainingTime % hour) / minute);
-  const textSecond = Math.floor((remainingTime % minute) / second);
+  const textHour = pad(Math.floor((remainingTime % day) / hour));
+  const textMinute = pad(Math.floor((remainingTime % hour) / minute));
+  const textSecond = pad(Math.floor((remainingTime % minute) / second));
   updateTimerUI(textDay, textHour, textMinute, textSecond);
 };
 
 // should use 500 as setInterval won't always run on time.
-setInterval(countdown, 500);
+setInterval(tick, 500);
